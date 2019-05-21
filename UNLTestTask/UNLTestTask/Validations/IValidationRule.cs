6ac8a26652222a9c1f01e913f88d8b1226a3f620
Pavel Efimenko <p.efimenko@ydt-global.com>
2019-05-21 16:44:16 +0300

    Added validation pattern Added rules for checking the validity of contact

---
+++ UNLTestTask/UNLTestTask/Validations/IValidationRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UNLTestTask.Validations
+{
+	public interface IBaseValidationRule<in T>
+	{
+		IReadOnlyList<string> Properties { get; }
+		string ValidationMessage { get; set; }
+		bool Check(T value);
+	}
+}
---
+++ UNLTestTask/UNLTestTask/Validations/IValidity.cs
@@ -0,0 +1,7 @@
+﻿namespace UNLTestTask.Validations
+{
+	public interface IValidity
+	{
+		bool IsValid { get; set; }
+	}
+}
